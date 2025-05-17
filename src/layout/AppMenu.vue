ace479140a174fc5234b9f7952dd7e59bb01422a
Wilver Vargas <kryshot05@gmail.com>
2025-05-17 08:42:59 -0400

    chore: add files to layout

---
+++ src/layout/AppMenu.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+import AppMenuItem from './AppMenuItem.vue'
+
+const model = ref([
+  {
+    label: 'Pages',
+    icon: 'pi pi-fw pi-briefcase',
+    items: [
+      //   {
+      //     label: 'Landing',
+      //     icon: 'pi pi-fw pi-globe',
+      //     to: '/landing',
+      //   },
+      //   {
+      //     label: 'Crud',
+      //     icon: 'pi pi-fw pi-pencil',
+      //     to: '/pages/crud',
+      //   },
+      {
+        label: 'Empty',
+        icon: 'pi pi-fw pi-circle-off',
+        to: '/pages/empty',
+      },
+    ],
+  },
+])
+</script>
+
+<template>
+  <ul class="layout-menu">
+    <template v-for="(item, i) in model" :key="item">
+      <AppMenuItem :item="item" root :index="i" />
+
+      <li class="menu-separator"></li>
+    </template>
+  </ul>
+</template>
+
+<style lang="scss" scoped></style>
